45256c449786af06ec9ed21441341f97d7ec509d
Zarif Aziz <zarifaziz08@gmail.com>
2024-08-04 22:32:37 +1000

    dev

--- src/bot/settings.py
+++ src/bot/settings.py
@@ -2,7 +2,6 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    GOOGLE_API_KEY: str = ""
     OPENAI_API_KEY: str = ""
     TELEGRAM_BOT_TOKEN: str = ""
     VECTORSHIFT_API_KEY: str = ""
